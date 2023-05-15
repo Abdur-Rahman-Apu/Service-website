fc99e69f2e11883c3435e7b3330cfb8e5884db9f
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-05-15 19:22:30 +0600

    project section is added

--- js/index.js
+++ js/index.js
@@ -63,9 +63,19 @@ scrollDiv.addEventListener("mouseout", function () {
 });
 
 // dynamic typing (typed.js)
+
 const typed = new Typed(".type", {
   strings: ["Freelancer", "Designer", "Developer", "Content Writer"],
   typeSpeed: 50,
   backSpeed: 50,
   loop: true,
+});
+
+// counter up
+
+$(document).ready(function () {
+  $(".project-stats .counter").counterUp({
+    delay: 10,
+    time: 1500,
+  });
 });
