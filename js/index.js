e0f4377c43e89769da6ccb60fadda9b53194f6ac
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-05-23 15:10:34 +0600

    Dynamic typing changed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // dynamic typing (typed.js)
 
 const typed = new Typed(".type", {
-  strings: ["Freelancer", "Designer", "Developer", "Content Writer"],
+  strings: ["Freelancer", "Graphic Designer", "Web Developer", "Certified Trainer","Digital Marketer","Motion Graphics Designer"],
   typeSpeed: 50,
   backSpeed: 50,
   loop: true,
@@ -9,12 +9,37 @@ const typed = new Typed(".type", {
 
 // counter up
 
-$(document).ready(function () {
-  $(".counter").counterUp({
-    delay: 10,
-    time: 400,
-  });
-});
+// $(document).ready(function () {
+//   $(".counter").counterUp({
+//     delay: 10,
+//     time: 400,
+//   });
+// });
+
+let nums = document.querySelectorAll(".statistics-info .counter");
+        let section = document.querySelector(".statistics-detail");
+        let started = false; // Function Started ? No
+
+        window.onscroll = function () {
+           
+            if ((window.scrollY+500) >= section.offsetTop) {
+
+                if (!started) {
+                    nums.forEach((num) => startCount(num));
+                }
+                started = true;
+            }
+        };
+
+        function startCount(el) {
+            let goal = el.dataset.goal;
+            let count = setInterval(() => {
+                el.textContent++;
+                if (el.textContent == goal) {
+                    clearInterval(count);
+                }
+            }, 2000 / goal);
+        }
 
 // all courses
 
